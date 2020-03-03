f0a05d578aca3496807e60190d6f3af86dd0cb7f
Cameron <cprib99@gmail.com>
2020-03-03 09:00:20 -0500

    added alg 2

--- main.cc
+++ main.cc
@@ -12,6 +12,8 @@ const uint16_t
   IS_FILLED_IN = 0x8000,
   IS_O_VALID = 0x2000;
 
+bool findBest(uint16_t puzzle);
+
 int main()
 {
     //read data using cin
@@ -28,6 +30,8 @@ int main()
       }
     }
 
+
+
     /*
     for (int r = 0; r < 9; r++)
     {
@@ -37,4 +41,49 @@ int main()
       }
     }
     */
+}
+
+bool findBest(uint16_t puzzle, /*stack*/)
+{
+  //if cell empty, mark all digits valid
+  for (int r = 0; r < 9; r++)
+  {
+    for (int c = 0; c < 9; c++)
+    {
+      if (puzzle[r][c] = '0')//cell empty
+        ;  //mark all digits as valid choices
+    }
+  }
+
+  //if cell filled in, mak digit as invalid for row/column/3x3
+  for (int r = 0; r < 9; r++)
+  {
+    for (int c = 0; c < 9; c++)
+    {
+
+    }
+  }
+
+  int low = 10;
+  for (int r = 0; r < 9; r++)
+  {
+    for (int c = 0; c < 9; c++)
+    {
+      if (puzzle[r][c] = '0')
+        //count 1-bits in valid choices for board[i][j]???
+        //for (int count )
+        if (count < low)
+        {
+          low = count;
+          int iBest = i;
+          int jBest = j;
+        }
+    }
+  }
+  if low = 10
+    return false;
+
+  //Mark board[iBest][jBest] as filled in
+  //push(iBest,jBest onto stack)
+  return true;
 }
